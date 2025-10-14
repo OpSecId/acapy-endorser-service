0697d429dca180932cb9b9bfd04d9be7fb973c86
Patrick St-Louis <patrick.st-louis@opsecid.ca>
2025-10-14 02:17:35 -0400

    move config routes
    Signed-off-by: Patrick St-Louis <patrick.st-louis@opsecid.ca>

--- endorser/api/endpoints/routes/allow.py
+++ endorser/api/endpoints/routes/allow.py
@@ -13,13 +13,11 @@ Usage:
 
 """
 
-from codecs import iterdecode
 import logging
-from csv import DictReader
-from typing import Annotated, Optional, TypeVar
+from typing import Optional, TypeVar
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import delete, select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -363,137 +361,6 @@ async def delete_allowed_cred_def(
         raise HTTPException(status_code=db_to_http_exception(e), detail=str(e))
 
 
-def maybe_str_to_bool(s: str) -> str | bool:
-    """Convert string to boolean if applicable."""
-    return s == "True" if isinstance(s, str) else s
-
-
-def construct_allowed_credential_definition(cd: dict) -> AllowedCredentialDefinition:
-    """Construct AllowedCredentialDefinition with proper boolean conversion."""
-    cd["rev_reg_def"] = maybe_str_to_bool(cd["rev_reg_def"])
-    cd["rev_reg_entry"] = maybe_str_to_bool(cd["rev_reg_entry"])
-    ncd = AllowedCredentialDefinition(**cd)
-    return ncd
-
-
-async def update_allowed_config(k, v, db):
-    """Update the allowed configuration in the database with entries from a CSV file.
-
-    This function reads a CSV file, constructs instances of specified classes,
-    and adds them to the database.
-
-    Args:
-        k: An object with attributes 'file' (CSV file handle)
-           and 'filename' (name of the CSV file).
-        v: A class type, used to construct instances from CSV data.
-        db: Database session to which the constructed instances are added.
-
-    Returns:
-        A dictionary containing the filename and list of constructed class instances.
-
-    """
-    csvReader = DictReader(iterdecode(k.file, "utf-8"))
-    constructed_classes = [
-        (
-            construct_allowed_credential_definition(i)
-            if v is AllowedCredentialDefinition
-            else v(**i)
-        )
-        for i in csvReader
-    ]
-    tmp = {
-        "file_name": k.filename,
-        "contents": constructed_classes,
-    }
-    for i in tmp["contents"]:
-        db.add(i)
-    return tmp
-
-
-async def update_full_config(
-    log_entry: Optional[UploadFile],
-    publish_did: Optional[UploadFile],
-    schema: Optional[UploadFile],
-    credential_definition: Optional[UploadFile],
-    db: AsyncSession,
-    delete_contents: bool,
-) -> dict:
-    """Update full configuration, possibly deleting existing entries."""
-    correlated_tables = {
-        log_entry: AllowedLogEntry,
-        publish_did: AllowedPublicDid,
-        schema: AllowedSchema,
-        credential_definition: AllowedCredentialDefinition,
-    }
-    modifications = {}
-    for k, v in correlated_tables.items():
-        if k:
-            if delete_contents:
-                await db.execute(delete(v))
-            modifications[v.__name__] = await update_allowed_config(k, v, db)
-    await db.commit()
-    await updated_allowed(db)
-    return modifications
-
-
-@router.post(
-    "/config",
-    status_code=status.HTTP_200_OK,
-    response_model=dict,
-    description="Upload a new CSV config replacing the existing configuration.",
-)
-async def set_config(
-    log_entry: Annotated[
-        UploadFile, File(description="List of log entries authorized to be published")
-    ] = None,
-    publish_did: Annotated[
-        UploadFile, File(description="List of DIDs authorized to become public")
-    ] = None,
-    schema: Annotated[
-        UploadFile, File(description="List of schemas authorized to be published")
-    ] = None,
-    credential_definition: Annotated[
-        UploadFile, File(description="List of creddefs authorized to be published")
-    ] = None,
-    db: AsyncSession = Depends(get_db),
-) -> dict:
-    """Set new configuration by uploading CSVs, replacing the existing configuration."""
-    try:
-        return await update_full_config(
-            log_entry, publish_did, schema, credential_definition, db, True
-        )
-    except Exception as e:
-        raise HTTPException(status_code=db_to_http_exception(e), detail=str(e))
-
-
-@router.put(
-    "/config",
-    status_code=status.HTTP_200_OK,
-    response_model=dict,
-    description="Upload a new CSV config appending to the existing configuration.",
-)
-async def append_config(
-    log_entry: Annotated[
-        UploadFile, File(description="List of log entries authorized to be published")
-    ] = None,
-    publish_did: Annotated[
-        UploadFile, File(description="List of DIDs authorized to become public")
-    ] = None,
-    schema: Annotated[
-        UploadFile, File(description="List of schemas authorized to be published")
-    ] = None,
-    credential_definition: Annotated[
-        UploadFile, File(description="List of authorized creddefs")
-    ] = None,
-    db: AsyncSession = Depends(get_db),
-) -> dict:
-    """Append new configuration by uploading CSVs to the existing configuration."""
-    try:
-        return await update_full_config(
-            log_entry, publish_did, schema, credential_definition, db, False
-        )
-    except Exception as e:
-        raise HTTPException(status_code=db_to_http_exception(e), detail=str(e))
 
 
 @router.get(
